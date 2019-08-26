76190f77e567c890f95bd38907a862a461579727
RishkoSergey <risko_sergey@mail.ru>
2019-08-26 15:55:41 +0300

    server requests for users page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import style from './Users.module.css';
 import User from './User/User';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 const Users = (props) => {
-    console.log(props.users);
-    let usersElements = props.users.map(u => <User name = {u.name} city = {u.city} followed = {u.followed}
-                                                   id = {u.id} status = {u.status} photo = {u.photo}
-                                                   follow = {props.follow} unfollow = {props.unfollow} />);                                              
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        })
+    }
+
+    // if (props.users.length === 0){
+    //     props.setUsers([
+    //             {id: 1, name: 'Gangster', city: 'Karaganda', followed: true, 
+    //             status: 'I am not the richest, smartest or most talented person in the world, but I succeed because I keep going and going and going',
+    //             photo: 'https://www.meme-arsenal.com/memes/f47cf328dc07b527192f75ef1aea1ad2.jpg'},
+
+    //             {id: 2, name: 'Hustler', city: 'Astana', followed: false, 
+    //             status: 'Life ain\'t about how hard you hit. It\'s about how hard you can get hit and keep moving forward',
+    //             photo: 'http://1000chertej.ru/wp-content/uploads/2017/09/19/22203/Bezrodnyj-1.jpg'},
+
+    //             {id: 3, name: 'Mafiozi', city: 'Almaty', followed: true, 
+    //             status: 'Success is usually the culmination of controlling failure', 
+    //             photo: 'https://pixanews.com/wp-content/uploads/2015/01/CahCats-pixanews-2-13.jpg'}]
+    //     );
+    // }
+    
+    let usersElements = props.users.map(u => <User name={u.name} city={'u.city'} followed={u.followed}
+        id={u.id} status={'u.status'} photo={u.photos.small != null ? u.photos.small : userPhoto}
+        follow={props.follow} unfollow={props.unfollow} />);
     return (
         <div>
             {usersElements}
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,19 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
 
 let initialState = {
-    users: [
-        {id: 1, name: 'Gangster', city: 'Karaganda', followed: true, 
-        status: 'I am not the richest, smartest or most talented person in the world, but I succeed because I keep going and going and going',
-        photo: 'https://www.meme-arsenal.com/memes/f47cf328dc07b527192f75ef1aea1ad2.jpg'},
-
-        {id: 2, name: 'Hustler', city: 'Astana', followed: false, 
-        status: 'Life ain\'t about how hard you hit. It\'s about how hard you can get hit and keep moving forward',
-        photo: 'http://1000chertej.ru/wp-content/uploads/2017/09/19/22203/Bezrodnyj-1.jpg'},
-        
-        {id: 3, name: 'Mafiozi', city: 'Almaty', followed: true, 
-        status: 'Success is usually the culmination of controlling failure', 
-        photo: 'https://pixanews.com/wp-content/uploads/2015/01/CahCats-pixanews-2-13.jpg'}
-    ]
+    users: []
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -41,7 +29,7 @@ const usersReducer = (state = initialState, action) => {
                     })
                 } 
         case SET_USERS:
-            return {...state, users: [...state.users, action.users]}
+            return {...state, users: [...state.users, ...action.users]}
         default: return state;
     }
 }
